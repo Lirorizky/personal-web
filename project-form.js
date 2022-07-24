63fed2429dc394bdd18b7d93b2c1a42bf7b80d5b
Lirorizky <liro.zke@gmail.com>
2022-07-25 04:55:22 +0700

    duration

--- project-form.js
+++ project-form.js
@@ -58,7 +58,7 @@ function displayProject() {
                 <p>${projects[i].projectName}</p>
               </div>
               <div class="project-card-duration">
-                <p>Duration : 3 Months</p>
+                <p>${projectDurationTime(projects[i].projectStartDate, projects[i].projectEndDate)}</p>
               </div>
               <div class="project-card-desc">
                 <p>
@@ -81,6 +81,39 @@ function displayProject() {
       </div>`
   }
   alert('Your project has been successfully updated.');
+}
+
+function projectDurationTime(a, b){
+  let projectStartAt = new Date(a);
+  let projectEndAt = new Date(b);
+
+  let duration = projectEndAt - projectStartAt;
+
+  //day duration
+  
+  let yearDuration = Math.floor(duration / (1000 * 60 * 60 * 24 * 30 * 12));
+
+  if(yearDuration > 0){
+    return `Duration: ${yearDuration} years`;
+  } else {
+    let monthDuration = Math.floor(duration / (1000 * 60 * 60 * 24 * 30));
+
+    if (monthDuration > 0) {
+      return `Duration: ${monthDuration} months`;
+    } else {
+      let weeksDuration = Math.floor(duration / (1000 * 60 * 60 * 24 * 7));
+
+      if(weeksDuration > 0) {
+        return `Duration: ${weeksDuration} weeks`;
+      } else {
+        let dayDuration = Math.floor(duration / (1000 * 60 * 60 * 24));
+
+        if(dayDuration > 0) {
+          return `Duration: ${dayDuration} days`;
+        }
+      }
+    }
+  }
 }
 
 function firstProjectDisplay() {
